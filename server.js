c08afba9829fa41f1e1ba05d661b121e679b8379
nurul286 <Nurulalam@matchps.com>
2022-07-20 17:18:35 +0600

    Update server.js file code

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const http = require("http");
 const root = require("app-root-path");
 require("dotenv").config();
 
 const app = require(`${root}/index`);
-const server = http.createServer(app);
 
-const host = process.env.HOST;
 const port = process.env.PORT;
-server.listen(port, host);
+app.listen(port, () => {
+console.log(`Server listening on http://localhost:${port}`);
 
-console.log(`Server listening on http://${host}:${port}`);
+});
